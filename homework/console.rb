9373a5ea73fae1040f6afb392348895815f9052e
Craig Dunlop <craigdun9393@gmail.com>
2018-09-21 15:09:51 +0100

    created film class

--- homework/console.rb
+++ homework/console.rb
@@ -5,11 +5,25 @@ require_relative( 'models/ticket' )
 
 require( 'pry' )
 
-Customer.delete_all
+Customer.delete_all()
+Film.delete_all()
 
 customer1 = Customer.new("name" => "craig", "funds" => 100)
+customer1.save()
 customer2 = Customer.new("name" => "Eilidh", "funds" => 120)
+customer2.save()
 customer3 = Customer.new("name" => "Bob", "funds" => 70)
+customer3.save()
+
+film1 = Film.new("title"=> "A Simple Favour", "price" => 10)
+film1.save()
+film2 = Film.new("title"=> "Ant-man and the Wasp", "price" => 10)
+film2.save()
+film3 = Film.new("title"=> "Blackkklansman", "price" => 10)
+film3.save()
+film4 = Film.new("title"=> "Crazy Rich Asians", "price" => 10)
+film4.save()
+film5 = Film.new("title"=> "Incredibles 2", "price" => 10)
 
 
 
--- homework/models/film.rb
+++ homework/models/film.rb
@@ -1,2 +1,40 @@
+require_relative("../db/sql_runner")
+
 class Film
-end 
+  def initialize(options)
+    @id = options["id"].to_i if options["id"]
+    @title = options["title"]
+    @price = options["price"]
+  end
+
+  def save()
+    sql = "INSERT INTO films(title, price) VALUES ($1, $2) RETURNING id"
+    values = [@title, @price]
+    film = SqlRunner.run(sql, values).first
+    @id = film["id"].to_i
+  end
+
+  def self.all
+    sql = "SELECT * FROM films"
+    films = SqlRunner.run(sql)
+    result = films.map{|film| Film.new(film)}
+    return result
+  end
+
+  def self.delete_all
+    sql = "DELETE FROM films"
+    SqlRunner.run(sql)
+  end
+
+  def delete
+    sql = "DELETE FROM films WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql)
+  end
+
+  def update
+    sql = "UPDATE films SET (title, price) = ($1, $2) WHERE id = $3"
+    values = [@title, @price, @id]
+    SqlRunner.run(sql, values)
+  end
+end
