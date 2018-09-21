1df769a1dd9e9d566147ee5d695f7f931843694b
Craig Dunlop <craigdun9393@gmail.com>
2018-09-21 11:41:52 +0100

    created customer class

--- homework/models/customer.rb
+++ homework/models/customer.rb
@@ -1,2 +1,41 @@
+require_relative("../db/sql_runner")
+
 class Customer
-end 
+
+  def initialize(options)
+    @id = options["id"].to_i if options["id"]
+    @name = options["name"]
+    @funds = options["funds"].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO customers(name, funds) VALUES ($1, $2) RETURNING id"
+    values = [@name, @funds]
+    casting = SqlRunner.run(sql, values).first
+    @id = casting["id"].to_i
+  end
+
+  def self.all
+    sql = "SELECT * FROM customers"
+    customers = SqlRunner.run(sql)
+    result = customers.map{|customer| Customer.new(customer)}
+    return result
+  end
+
+  def self.delete_all
+    sql = "DELETE FROM customers"
+    SqlRunner.run(sql)
+  end
+
+  def delete
+    sql = "DELETE FROM customers WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql)
+  end
+
+  def update
+    sql = "UPDATE customers SET (name, funds) = ($1, $2) WHERE id = $3"
+    values = [@name, @funds, @id]
+    SqlRunner.run(sql, values)
+  end
+end
