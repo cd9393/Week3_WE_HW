0544ee1271f2cdcdffc0df93e367de8f9d664ad9
Craig Dunlop <craigdun9393@gmail.com>
2018-09-21 16:17:36 +0100

    added .customers to film class

--- homework/models/film.rb
+++ homework/models/film.rb
@@ -3,7 +3,7 @@ require_relative("../db/sql_runner")
 class Film
   attr_reader :id
   attr_accessor :title, :price
-  
+
   def initialize(options)
     @id = options["id"].to_i if options["id"]
     @title = options["title"]
@@ -15,6 +15,27 @@ class Film
     values = [@title, @price]
     film = SqlRunner.run(sql, values).first
     @id = film["id"].to_i
+  end
+
+  def delete
+    sql = "DELETE FROM films WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql)
+  end
+
+  def update
+    sql = "UPDATE films SET (title, price) = ($1, $2) WHERE id = $3"
+    values = [@title, @price, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def customers
+    sql = "SELECT customers.* FROM
+     tickets INNER JOIN screenings ON screenings.id = tickets.screening_id INNER JOIN customers ON customer_id = customers.id
+     WHERE film_id = $1"
+    values = [@id]
+    customers = SqlRunner.run(sql, values)
+    result = customers.map{|customer| Customer.new(customer)}
   end
 
   def self.all
@@ -29,15 +50,4 @@ class Film
     SqlRunner.run(sql)
   end
 
-  def delete
-    sql = "DELETE FROM films WHERE id = $1"
-    values = [@id]
-    SqlRunner.run(sql)
-  end
-
-  def update
-    sql = "UPDATE films SET (title, price) = ($1, $2) WHERE id = $3"
-    values = [@title, @price, @id]
-    SqlRunner.run(sql, values)
-  end
 end
