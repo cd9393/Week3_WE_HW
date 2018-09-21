70f4714a1e7e30df12fc6212008505406c3fe151
Craig Dunlop <craigdun9393@gmail.com>
2018-09-21 15:13:17 +0100

    added attr readers and accessors

--- homework/models/film.rb
+++ homework/models/film.rb
@@ -1,6 +1,9 @@
 require_relative("../db/sql_runner")
 
 class Film
+  attr_reader :id
+  attr_accessor :title, :price
+  
   def initialize(options)
     @id = options["id"].to_i if options["id"]
     @title = options["title"]
